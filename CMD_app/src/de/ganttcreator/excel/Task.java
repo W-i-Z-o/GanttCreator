93f403f3bace7a3a747a5c46f3b5015816b44e29
knabsche@web.de <W-i-Z-o>
2016-12-04 15:22:28 +0100

    Added key-attribute to Task objects

--- CMD_app/src/de/ganttcreator/excel/Task.java
+++ CMD_app/src/de/ganttcreator/excel/Task.java
@@ -4,20 +4,22 @@ import java.time.Duration;
 
 public class Task extends Item {
     Duration duration;
+    String key;
 
-    public Task(int id, String name, int outlineLvl, Duration duration) {
+    public Task(int id, String key, String name, int outlineLvl, Duration duration) {
         this.setId(id);
+        this.setKey(key);
         this.setName(name);
         this.setOutlineLvl(outlineLvl);
         this.duration = duration;
     }
     
-    public Task(int id, String name, int outlineLvl){
-        this(id,name,outlineLvl,null);
+    public Task(int id, String key, String name, int outlineLvl) {
+        this(id, key, name, outlineLvl, null);
     }
 
-    public Task(String name, Duration duration) {
-        this(-1, name, 3, duration);
+    public Task(String key, String name, Duration duration) {
+        this(-1, key, name, 3, duration);
     }
 
     public Duration getDuration() {
@@ -34,5 +36,13 @@ public class Task extends Item {
         long m = (seconds / 60) % 60;
         long h = (seconds / (60 * 60)) % 24;
         return String.format("PT%dH%dM%dS", h, m, s);
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
     }
 }
